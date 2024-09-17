5d0358c273b9e636398949e404bf41ba34c9d2cf
Karthick Thiyagarajan <karthickthiyagarajanofficial@gmail.com>
2024-09-17 21:14:15 +0530

    Delete SRM_TRP_Chatbot directory

--- SRM_TRP_Chatbot/src/utils/helpers.ts
+++
@@ -1,9 +0,0 @@
-export const formatDate = (date: Date): string => {
-  return date.toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
-};
-
-// Add more utility functions as needed
